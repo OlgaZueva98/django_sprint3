b4c3ed5e56a29b76baf565ef1dd0b84390c6d75b
zuevaom <omzueva@vei.ru>
2025-02-12 15:31:10 +0300

    Исправлены файлы admin.py, models.py, views.py приложения blog

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-# from django.db.models import Q
 from blog.models import Post, Category
 from datetime import datetime
 
 
-def index(request):
-    post_list = Post.objects.filter(
-        pub_date__lt=datetime.now(),
+def query():
+    result = Post.objects.select_related(
+        'category',
+        'author',
+        'location'
+    ).filter(
         is_published=True,
+        pub_date__lt=datetime.now(),
         category__is_published=True
-    ).order_by('-pub_date')[0:5]
+    )
+
+    return result
+
+
+def index(request):
+    post_list = query().order_by('-pub_date')[:5]
+
     return render(request, 'blog/index.html', {'post_list': post_list})
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(
-        Post.objects.filter(
-            pub_date__lt=datetime.now(),
-            is_published=True,
-            category__is_published=True
-        ),
+        query(),
         pk=post_id
     )
     return render(request, 'blog/detail.html', {'post': post})
@@ -30,10 +36,8 @@ def category_posts(request, category_slug):
         Category.objects.filter(is_published=True),
         slug=category_slug
     )
-    post_by_category = Post.objects.filter(
-        category__slug=category_slug,
-        is_published=True,
-        pub_date__lt=datetime.now()
+    post_by_category = query().filter(
+        category__slug=category_slug
     )
     return render(request, 'blog/category.html',
                   {'category': category, 'post_list': post_by_category})
--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -7,6 +7,6 @@ app_name = 'pages'
 urlpatterns = [
     # Если вызван URL без относительного адреса (шаблон — пустые кавычки),
     # то вызывается view-функция index() из файла views.py
-    path('about/', views.about, name="about"),
+    path('about/', views.about, name='about'),
     path('rules/', views.rules, name='rules'),
 ]
